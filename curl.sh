cdf8860c89ba199aca697290fc1d9143407d347d
Tom Weinert <tomasz.weinert@cineworldgroup.com>
2023-03-24 13:34:53 +0100

    Cleanup

--- curl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-tm=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-STRONA=`w3m -dump tvp.info`
-ILETUSK=`w3m -dump tvp.info | grep -ic tusk`
-ILEMORAWIECKI=`w3m -dump tvp.info | grep -ic morawieck`
-ILEKACZYNSKI=`w3m -dump tvp.info | grep -ic kaczyńsk`
-echo "${tm},\"${ILETUSK}\",\"${ILEMORAWIECKI}\",\"${ILEKACZYNSKI}\""
